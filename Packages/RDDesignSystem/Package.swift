429d3bf88010499d53bc84bfb2d3e3e0948a2a51
Pranav Suri <pranav.suri@ecobee.com>
2024-08-03 17:47:33 -0400

    remove example app from project.

--- Packages/RDDesignSystem/Package.swift
+++ Packages/RDDesignSystem/Package.swift
@@ -7,31 +7,21 @@ let package = Package(
         .iOS(.v15),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RDDesignSystem",
             targets: ["RDDesignSystem"]),
     ],
-    dependencies: [
-      .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.0")
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RDDesignSystem",
             dependencies: [],
-            path: "Sources", 
-            resources: [
-                .process("Resources")
-            ] 
-//            plugins: [
-//                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin")
-//            ]
+            path: "Sources"
         ),
-        
         .testTarget(
             name: "RDDesignSystemTests",
-            dependencies: ["RDDesignSystem"]),
+            dependencies: ["RDDesignSystem"],
+            path: "Tests"
+        ),
     ]
 )
--- Packages/RDDesignSystem/Sources/Extensions/Color + Extensions.swift
+++ Packages/RDDesignSystem/Sources/Extensions/Color + Extensions.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import UIKit
 
 @available(iOS 13.0, *)
 public extension Color {
